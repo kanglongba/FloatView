8d34c9154a7014ab5216a367e174843551656587
edison <edison.qian@applovin.com>
2022-10-05 17:13:15 +0800

    添加打开设置页面的代码

--- app/src/main/java/com/applovin/me/floatview/MainActivity.java
+++ app/src/main/java/com/applovin/me/floatview/MainActivity.java
@@ -1,10 +1,20 @@
 package com.applovin.me.floatview;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.applovin.me.floatview.databinding.ActivityMainBinding;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +35,49 @@ public class MainActivity extends AppCompatActivity {
         binding.hideBall.setOnClickListener(v -> {
 
         });
+        binding.startDemoBtn.setOnClickListener(v -> {
+            startSettings("com.clipwiregames.wordbuddies");
+
+        });
     }
 
+    /**
+     * 根据包名，打开目标app，需要package权限。如果没有package权限，则无效
+     * https://developer.android.com/training/package-visibility
+     * @param packageName
+     */
+    public void launchApp(String packageName) {
+        Intent intent = new Intent();
+        intent.setPackage(packageName);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
+        Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(pm));
+
+        if(resolveInfos.size() > 0) {
+            ResolveInfo launchable = resolveInfos.get(0);
+            ActivityInfo activity = launchable.activityInfo;
+            ComponentName name=new ComponentName(activity.applicationInfo.packageName,
+                    activity.name);
+            Intent i=new Intent(Intent.ACTION_MAIN);
+
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                    Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            i.setComponent(name);
+
+            startActivity(i);
+        }
+    }
+
+    /**
+     * 打开软件设置页面
+     * @param packageName
+     */
+    public void startSettings(String packageName) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", packageName, null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
 }
